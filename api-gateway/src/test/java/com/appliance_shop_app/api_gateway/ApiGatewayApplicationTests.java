cad3f18f78f286139e7f6906820ed16fb067ab7e
SebastianSerranoB <sebastianserranob11@gmail.com>
2025-02-27 17:09:11 -0300

    first commit

---
+++ api-gateway/src/test/java/com/appliance_shop_app/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appliance_shop_app.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ shopping-cart-service/src/test/java/com/appliance_shop_app/shopping_cart_service/ShoppingCartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appliance_shop_app.shopping_cart_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
